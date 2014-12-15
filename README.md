4f5ca044263e37fb152ab1f517ff7bebf12008de
Borys Timakov <borys.b.timakov@gmail.com>
2014-12-15 11:04:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Synopsis
+
+Jasper Reports Shell documentation
+
+
+## Motivation
+
+I was missing the ability to contribute into the original repository for JRSH, so I made this temporary repository for the documentation of the tool.
+
+## Installation
+
+Please use FreeMind or MindNode mindmapping software for being able to read the mindmaps. So far PDF version is always available too.
---
+++ docs/jrsh_mindmap.mm
@@ -0,0 +1,195 @@
+
+<map version="0.9.0">
+    <node ID="3C39D2BC-5D63-459C-BC1C-C9B7EA35F29A" TEXT="jrsh" FOLDED="false" COLOR="#157A96">
+        <font NAME="HelveticaNeue-Light" SIZE="21"></font>
+        <edge COLOR="#F6F6F6"></edge>
+        <node ID="8FD5A763-E470-46E1-9175-AB9C9BCD27F4" TEXT="parameters" POSITION="right" FOLDED="false" COLOR="#5B5B5B">
+            <font NAME="HelveticaNeue" SIZE="18"></font>
+            <edge COLOR="#AF36F2"></edge>
+            <node ID="F5A29FEB-662F-48B0-B127-296BF39DFD9E" TEXT="-username" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#AF36F2"></edge>
+                <node ID="218EBA0E-E3AC-46D0-B893-12B78BBD6579" TEXT="JRS username credential" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#AF36F2"></edge>
+                </node>
+            </node>
+            <node ID="8D09A810-BE11-4001-9B93-C16D11939392" TEXT="-password" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#AF36F2"></edge>
+                <node ID="1983288B-580E-4DCB-B606-8F53170666F9" TEXT="JRS password" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#AF36F2"></edge>
+                </node>
+            </node>
+            <node ID="1DC7B63D-6CBE-4BDA-A02B-B3646CE2844B" TEXT="-organization" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#AF36F2"></edge>
+                <node ID="4D4324B3-0075-43DF-94CD-EE83B0409C74" TEXT="JRS tenant name (Commercial edition only)" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#AF36F2"></edge>
+                </node>
+            </node>
+            <node ID="E09F131C-FDDB-42B4-AA0C-7F38EED53ABA" TEXT="-server" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#AF36F2"></edge>
+                <node ID="474438CD-325B-4CD6-B64C-D2CC269A47FA" TEXT="JRS Instance access URL" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#AF36F2"></edge>
+                </node>
+            </node>
+            <node ID="E3E41514-B13C-4704-AC5C-2FBF7E5ABC6A" TEXT="-debug" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#AF36F2"></edge>
+                <node ID="5263391F-45EC-4237-9ABF-F6F0BE05BE66" TEXT="debug action" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#AF36F2"></edge>
+                </node>
+            </node>
+        </node>
+        <node ID="EA1684CF-1893-4A4F-8AF3-F8CE50D2E9BB" TEXT="exit codes" POSITION="left" FOLDED="false" COLOR="#5B5B5B">
+            <font NAME="HelveticaNeue" SIZE="18"></font>
+            <edge COLOR="#FFBD0A"></edge>
+            <node ID="F0DBBE28-FBF9-41A1-BB1F-101C3A6396BA" TEXT="0" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#FFBD0A"></edge>
+                <node ID="96D414AB-15F5-40CE-8360-004876F47262" TEXT="SUCCESS" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#FFBD0A"></edge>
+                </node>
+            </node>
+            <node ID="D4963340-35BE-4DA2-B63F-10F331DE19B4" TEXT="non 0" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#FFBD0A"></edge>
+                <node ID="73BAEC20-D2CF-4DD8-89C7-6E149903FD6F" TEXT="TODO" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#FFBD0A"></edge>
+                </node>
+            </node>
+        </node>
+        <node ID="B2E796EA-16A8-456C-9538-A4C92751C130" TEXT="actions" POSITION="right" FOLDED="false" COLOR="#5B5B5B">
+            <font NAME="HelveticaNeue" SIZE="18"></font>
+            <edge COLOR="#24AAFF"></edge>
+            <node ID="504634E4-98F4-4A00-98C5-A56CCC88B9F6" TEXT="help | ?" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#24AAFF"></edge>
+            </node>
+            <node ID="9166EDC8-E740-427D-9621-666D2907A326" TEXT="import" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#24AAFF"></edge>
+                <node ID="52C183C6-D510-4A03-ABAF-7CF1929D38A5" TEXT="&lt;path&gt;" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                    <node ID="567B6C36-AEFA-438A-A235-D11DFC97164B" TEXT="without-update*" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="ADD0AA93-608C-4976-9EEC-43986333776C" TEXT="without-events*" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="27F6EE92-7853-4760-AC0F-9B78E0D35877" TEXT="with-audit-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="056F0274-4479-4420-A8D7-D834FE53784E" TEXT="with-access-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="F71BFF65-6672-41D8-AE73-41AA46C88788" TEXT="with-monitoring-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="9C7B66BE-1638-4F5E-A75A-7E129C4329C4" TEXT="with-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="C6521805-5FA6-4898-964C-B7DC9C48B6F6" TEXT="with-update" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                        <node ID="AF20306D-104E-48BD-A155-CC987604C038" TEXT="with-skip-user-update" FOLDED="false" COLOR="#5B5B5B">
+                            <font NAME="HelveticaNeue" SIZE="18"></font>
+                            <edge COLOR="#24AAFF"></edge>
+                        </node>
+                    </node>
+                </node>
+            </node>
+            <node ID="16069685-523E-4103-824A-FE42AD972E83" TEXT="export" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#24AAFF"></edge>
+                <node ID="9FDB0F4C-AE12-4737-B9A1-DB2AD3AD7779" TEXT="everything*" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                </node>
+                <node ID="8B9A0B79-76E5-4EC1-9F2A-412757C28063" TEXT="&lt;path&gt;" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                    <node ID="74C2B9EA-AFAB-4D90-B31D-A80A9112BE7B" TEXT="with-everything*" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="45161939-CDBA-426D-8882-550591AB67A2" TEXT="without-users-and-roles" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="72762B48-C18A-48A1-9925-7D1A7E4BE538" TEXT="without-access-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="E45CCD87-24E3-40B1-B6CE-DFE54B400A34" TEXT="without-audit-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="00D607E1-7BEE-450F-AF4E-A73F9B9DCD10" TEXT="without-monitoring-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="F0A4B64B-739C-40CB-9A95-CD3F9A40E914" TEXT="without-events" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                </node>
+                <node ID="FC2C2599-2574-4075-B7CE-57C99915C808" TEXT="users" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                </node>
+                <node ID="B45A622F-6D0C-4088-B38A-9D4DB53C76D6" TEXT="roles" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                </node>
+                <node ID="F861AA8A-8617-4F18-BBB3-E05B97CA832E" TEXT="events" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                    <node ID="F4584767-DFAD-43E2-8601-40B2B41627F1" TEXT="all*" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="A0D4CF98-96AC-4849-A67B-75CCCDC059C7" TEXT="access" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="E1D25AD5-19BA-4EDE-BA21-2096BD94A04F" TEXT="audit" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                    <node ID="21B1CAD0-83FA-42F4-930F-5CE87A2C6A5D" TEXT="monitoring" FOLDED="false" COLOR="#5B5B5B">
+                        <font NAME="HelveticaNeue" SIZE="18"></font>
+                        <edge COLOR="#24AAFF"></edge>
+                    </node>
+                </node>
+            </node>
+            <node ID="150936E8-E0FB-457E-92BB-06CDC073B469" TEXT="show" FOLDED="false" COLOR="#5B5B5B">
+                <font NAME="HelveticaNeue" SIZE="18"></font>
+                <edge COLOR="#24AAFF"></edge>
+                <node ID="B878E087-8014-4BC9-925B-4081CC1D158A" TEXT="server-info" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                </node>
+                <node ID="ACF2A154-3298-4575-BAC7-1BEE06A2067F" TEXT="supported" FOLDED="false" COLOR="#5B5B5B">
+                    <font NAME="HelveticaNeue" SIZE="18"></font>
+                    <edge COLOR="#24AAFF"></edge>
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
